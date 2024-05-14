89c7c62bf2a0ca3705f9644102afe5bbe1cf23c6
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-05-14 15:20:58 +0600

    setup singel product api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -43,6 +43,13 @@ async function run() {
     app.get('/products', async (req,res) => {
       const cursor = porductCollection.find();
       const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/query/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await porductCollection.findOne(query)
       res.send(result)
     })
 
