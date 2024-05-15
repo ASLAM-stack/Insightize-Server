c8e1a265e471228cf35607d3d039ad48227ed263
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-05-15 23:26:15 +0600

    complete

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.qcl7kvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,7 +28,33 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const logger = (req, res, next) => {
+  console.log('log info', req.method, req.url);
+  next();
 
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log('token:', token);
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    req.user = decoded
+    next();
+  })
+}
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +62,40 @@ async function run() {
     // Send a ping to confirm a successful connection
     const porductCollection = client.db('ProductsDB').collection('products');
     const recommendCollection = client.db('ProductsDB').collection('Recommended');
+    
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d', })
+      res
+        .cookie('token', token, cookieOptions)
+        .send({ success: true })
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.user;
+
+      res.clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    })
+
+    app.get("/my-job-list", logger, verifyToken, async (req, res) => {
+      console.log(req.query?.email);
+      console.log('From my list', req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'Forbidden excess' })
+      }
+      const query = { employerEmail: req.query?.email }
+      const result = await JobCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/my_recommendation/:email',async(req,res) => {
+      const email = req.params.email;
+      const query = {
+        RecommenderEmail: email}
+      const result = await recommendCollection.find(query).toArray()
+      res.send(result)
+    })
     
     app.post(('/recommend'), async(req,res) =>{
       const recommend = req.body;
@@ -86,11 +154,24 @@ async function run() {
       const result = await recommendCollection.find(query).toArray()
       res.send(result)
     })
-    app.get('/my_recommendation/:email',async(req,res) => {
-      const email = req.params.email;
-      const query = {
-        RecommenderEmail: email}
-      const result = await recommendCollection.find(query).toArray()
+    
+
+    app.put('/update/:id',async(req,res) => {
+      const id = req.params.id;
+      const info = req.body;
+      const filter = {_id: new ObjectId(id)}
+      const option = {upsert:true}
+      const Updateinfo = {
+        $set:{
+          product_Name:info.product_Name,
+          product_Brand:info.product_Brand,
+          product_Image:info.product_Image,
+          query_Title:info.query_Title
+          ,boycott_Reason:info.boycott_Reason,
+          currentDateAndTime:info.currentDateAndTime
+        }
+      }
+      const result = await porductCollection.updateOne(filter,Updateinfo,option)
       res.send(result)
     })
 
@@ -99,6 +180,14 @@ async function run() {
       const query = {queryID:id}
       const result = await recommendCollection.find(query).toArray()
       res.send(result)
+    })
+
+    app.delete('/recommended/:id',async (req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await recommendCollection.deleteOne(query)
+      res.send(result)
+
     })
 
     await client.db("admin").command({ ping: 1 });
